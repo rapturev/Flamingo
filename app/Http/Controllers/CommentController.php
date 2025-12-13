565e0bf0d92820cf37d831d593a0ba6e82d83df2
Kirill Monakhov <withewings0@gmail.com>
2025-12-13 21:43:10 +0300

    Updated CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,5 +6,35 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+
 }
