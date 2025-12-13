23cd0918d363aa6f5adaaa2d4435b5afb79c933b
Kirill Monakhov <withewings0@gmail.com>
2025-12-13 22:41:53 +0300

    Added web-routes refered to Blog

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\FeedController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FeedController::class, 'index'])->name('feed.index');
+
+Route::prefix('blogs')->group(function() {
+    Route::get('/', [BlogController::class, 'index'])->name('blogs.index');
+    Route::get('/create', [BlogController::class, 'create'])->name('blogs.create');
+    Route::post('/{blog}', [BlogController::class, 'store'])->name('blogs.store');
+    Route::get('/{blog}', [BlogController::class, 'show'])->name('blogs.show');
+    Route::get('/{blog}/edit' [BlogController::class, 'edit'])->name('blogs.edit');
+    Route::put('/{blog}', [BlogController::class, 'update'])->name('blogs.update');
+    Route::delete('/{blog}', [BlogController::class, 'destroy'])->('blogs.destroy');
+})
