6cb64f839a52e809327c34e1ca3e26ba4f6183ca
Kirill Monakhov <withewings0@gmail.com>
2025-12-13 17:05:35 +0300

    Updated BookmarkController

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class BookmarkController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function mark()
+    {
+
+    }
+
+    public function unmark()
+    {
+
+    }
 }
