5660663c679432da755bd6035769632576b873a7
Kirill Monakhov <withewings0@gmail.com>
2025-12-13 16:49:37 +0300

    Updated BlogController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
 }
