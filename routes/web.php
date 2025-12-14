5902770dd03b80720f9295c7b144f5cb775c4a17
Kirill Monakhov <withewings0@gmail.com>
2025-12-14 17:47:12 +0300

    Added web-routes related to Post

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,13 @@ Route::prefix('blogs')->group(function() {
     Route::put('/{blog}', [BlogController::class, 'update'])->name('blogs.update');
     Route::delete('/{blog}', [BlogController::class, 'destroy'])->('blogs.destroy');
 })
+
+Route::prefix('posts')->group(function() {
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/{post}', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::get('/{post}/edit' [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/{post}', [PostController::class, 'destroy'])->('posts.destroy');
+})
