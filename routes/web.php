9e67af6903d22e54f9e78a2ff5df661c652e1762
Kirill Monakhov <withewings0@gmail.com>
2026-01-10 22:07:55 +0300

    Updated web-routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::prefix('blogs')->group(function() {
     Route::get('/create', [BlogController::class, 'create'])->name('blogs.create');
     Route::post('/{blog}', [BlogController::class, 'store'])->name('blogs.store');
     Route::get('/{blog}', [BlogController::class, 'show'])->name('blogs.show');
-    Route::get('/{blog}/edit' [BlogController::class, 'edit'])->name('blogs.edit');
+    Route::get('/{blog}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
     Route::put('/{blog}', [BlogController::class, 'update'])->name('blogs.update');
     Route::delete('/{blog}', [BlogController::class, 'destroy'])->('blogs.destroy');
 })
@@ -23,7 +23,7 @@ Route::prefix('posts')->group(function() {
     Route::get('/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('/{post}', [PostController::class, 'store'])->name('posts.store');
     Route::get('/{post}', [PostController::class, 'show'])->name('posts.show');
-    Route::get('/{post}/edit' [PostController::class, 'edit'])->name('posts.edit');
+    Route::get('/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
     Route::put('/{post}', [PostController::class, 'update'])->name('posts.update');
     Route::delete('/{post}', [PostController::class, 'destroy'])->('posts.destroy');
 })
