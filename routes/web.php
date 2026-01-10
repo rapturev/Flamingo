27136d4588476ed5351cc64eccb9657deda67533
Kirill Monakhov <withewings0@gmail.com>
2026-01-10 22:22:14 +0300

    Updated web-routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FeedController::class, 'index'])->name('feed.index');
 
+Route::prefix('users')->group(function() {
+    Route::get('/', [UserController::class, 'index'])->name('users.index');
+    Route::get('/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/{user}', [UserController::class, 'store'])->name('users.store');
+    Route::get('/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+});
+
 Route::prefix('blogs')->group(function() {
     Route::get('/', [BlogController::class, 'index'])->name('blogs.index');
     Route::get('/create', [BlogController::class, 'create'])->name('blogs.create');
@@ -32,3 +42,15 @@ Route::get('/setlike', [LikeController::class, 'setLike'])->name('likes.setlike'
 Route::get('/unsetlike', [LikeController::class, 'unsetLike'])->name('likes.unsetlike');
 
 Route::get('/tags', [TagController::class, 'index'])->name('tags.index');
+
+Route::prefix('walks')->group(function() {
+    Route::get('/', [WalkController::class, 'index'])->name('walks.index');
+    Route::get('/create', [WalkController::class, 'create'])->name('walks.create');
+    Route::post('/{walk}', [WalkController::class, 'store'])->name('walks.store');
+    Route::get('/{walk}', [WalkController::class, 'show'])->name('walks.show');
+    Route::get('/{walk}/edit', [WalkController::class, 'edit'])->name('walks.edit');
+    Route::put('/{walk}', [WalkController::class, 'update'])->name('walks.update');
+    Route::delete('/{walk}', [WalkController::class, 'destroy'])->name('walks.destroy');
+});
+
+
