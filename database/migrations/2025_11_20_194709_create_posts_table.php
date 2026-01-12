3a3593c4669e8c5eba4ae5d1689f3b812f2ec9db
Kirill Monakhov <withewings0@gmail.com>
2026-01-12 23:03:45 +0300

    Updated create_posts migration

--- database/migrations/2025_11_20_194709_create_posts_table.php
+++ database/migrations/2025_11_20_194709_create_posts_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('blog_id')->constrained();
+            $table->text('content');
         });
     }
 
