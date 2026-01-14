db303b9546fb51b03028ea49ebaaff1df1ecd46f
Kirill Monakhov <withewings0@gmail.com>
2026-01-14 13:46:10 +0300

    Updated create_walks_table migration

--- database/migrations/2025_12_12_131400_create_walks_table.php
+++ database/migrations/2025_12_12_131400_create_walks_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('walks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name');
         });
     }
 
