fd19efc8ddd97040e01ceacf4bbfe19f2974e1cc
Kirill Monakhov <withewings0@gmail.com>
2026-01-14 14:15:15 +0300

    Updated Bookmark model

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -9,4 +9,9 @@ class Bookmark extends Model
 {
     /** @use HasFactory<\Database\Factories\BookmarkFactory> */
     use HasFactory;
+
+    protected $fillable = [
+         'user_id',
+         'post_id',
+    ]
 }
