d5b28989d25cb74c8c8a470d97f4ee7c66c8ffc3
Kirill Monakhov <withewings0@gmail.com>
2026-01-14 13:18:57 +0300

    Updated create_post_tag_table migration

--- database/migrations/2026_01_14_100659_create_post_tag_table.php
+++ database/migrations/2026_01_14_100659_create_post_tag_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignId('post_id')->constrained();
+            $table->foreignId('tag_id')->constrained();
+            $table->primary(['post_id', 'tag_id']);
         });
     }
 
