728f3183dc5a1cb77716d7c096c0bad721b66697
Kirill Monakhov <withewings0@gmail.com>
2026-01-14 13:05:29 +0300

    Updated create_tags migration

--- database/migrations/2025_11_20_194646_create_tags_table.php
+++ database/migrations/2025_11_20_194646_create_tags_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->foreignId('post_id')->constrained();
             $table->string('name');
         });
     }
