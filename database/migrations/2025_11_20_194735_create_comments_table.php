06e321c432d633d88090b98c03af465c47e09222
Kirill Monakhov <withewings0@gmail.com>
2026-01-14 13:23:15 +0300

    Updated create_comments_table migration

--- database/migrations/2025_11_20_194735_create_comments_table.php
+++ database/migrations/2025_11_20_194735_create_comments_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('post_id')->constrained();
+            $table->foreignId('user_id')->constratied();
+            $table->text('content');
         });
     }
 
