9f08b5db58d414e58f84eccc99bacf0ed742d3d1
Kirill Monakhov <withewings0@gmail.com>
2026-01-14 13:39:58 +0300

    Updated create_bookmarks_table migration

--- database/migrations/2025_11_30_192234_create_bookmarks_table.php
+++ database/migrations/2025_11_30_192234_create_bookmarks_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('bookmarks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
         });
     }
 
