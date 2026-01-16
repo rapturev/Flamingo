83d227d8866b8bcc2441e4bc4bc005b5f2a6e10e
Kirill Monakhov <withewings0@gmail.com>
2026-01-16 13:47:45 +0300

    Updated UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function create() {
+
+    }
+
+    public function store() {
+
+    }
+
+    public function show() {
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function destroy() {
+
+    }
+
+
 }
