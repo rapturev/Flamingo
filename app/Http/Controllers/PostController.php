3b5d8a7b53db973cf957a02ef307fef83f59c12c
Kirill Monakhov <withewings0@gmail.com>
2026-01-31 22:44:40 +0300

    Updated PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,42 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index()
-    {
+    public function index() {}
 
+    public function create() {}
+
+    public function store(PostRequest $request)
+    {
+        $validated = $request->validate();
+        Post::create($validated);
     }
 
-    public function create()
-    {
+    public function show() {}
 
-    }
+    public function edit() {}
 
-    public function store()
-    {
+    public function update() {}
 
-    }
-
-    public function show()
-    {
-
-    }
-
-    public function edit()
-    {
-
-    }
-
-    public function update()
-    {
-
-    }
-
-    public function destroy()
-    {
-
-    }
+    public function destroy() {}
 }
